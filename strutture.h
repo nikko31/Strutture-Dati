cdeb55ff66db21b62f6cb5e5ceec7a75ec60a2af
nikko31 <nikko31@nikko31-HP>
2014-06-17 09:38:33 +0200

    new commit

--- strutture.h
+++ strutture.h
@@ -11,5 +11,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int boolean;
+typedef int Element;
+
+typedef struct list_element 
+{
+    Element value;
+    struct list_element *next;
+} item;
+typedef item* list;
+
+
+/* ---- PRIMITIVE ---- */
+extern list emptylist(void);
+extern boolean empty(list);/* Restituisce TRUE se l e` la lista vuota, FALSE altrimenti. */
+extern Element head(list);
+extern list tail(list);
+extern list cons(Element, list);
+//--------------------->
+extern Element copy(Element e);
+extern boolean isEqual(Element e1,Element e2);
+extern boolean isLess(Element e1, Element e2);
 #endif	/* STRUTTURE_H */
 
