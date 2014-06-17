f5e1fc7a8e692418dd8a70cdd43bcc47248c051b
nikko31 <nikko31@nikko31-HP>
2014-06-17 09:38:55 +0200

    new commit

--- strutture.c
+++ strutture.c
@@ -6,6 +6,52 @@
  */
 
 #include "strutture.h"
+//-------->PRIMITIVE------------------>
 
+list emptylist() 
+{
+    return NULL;
+}
 
+boolean empty(list l) 
+{
+    return (l==NULL);
+}
 
+Element head(list l) 
+{
+    if (empty(l)) abort();
+    else
+    return l->value;
+}
+
+list tail(list l) 
+{
+    if (empty(l)) abort();
+    else
+    return l->next;
+}
+
+list cons(Element e, list l) 
+{
+    list t;
+    t = (list) malloc(sizeof(item));
+    t->value=copy(e);
+    t->next=l;
+    return t;
+}
+//--------------------------->
+Element copy(Element e)
+{
+    Element el;
+    el=e;
+    return el;  
+}
+boolean isEqual(Element e1,Element e2) 
+{
+    return (e1 == e2);
+}
+boolean isLess(Element e1, Element e2)
+{
+return (e1 < e2);
+}
